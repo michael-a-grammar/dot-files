a3afbb7f8dc0da2f4c08fec1864b71d08804ff01
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-01-27 19:21:06 +0000

    Updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
   starship init fish | source
   source /opt/homebrew/opt/asdf/libexec/asdf.fish
+  source "$__fish_config_dir/base16-tomorrow-night.fish"
   abbreviations
 end
---
+++ setup.md
@@ -0,0 +1,1 @@
+brew install eth-p/software/bat-extras
