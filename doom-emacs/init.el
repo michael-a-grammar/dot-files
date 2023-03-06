8a851d82a1a98cf2727258a583aab2c80d3f1923
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-03-06 15:30:25 +0000

    Updates

--- doom-emacs/init.el
+++ doom-emacs/init.el
@@ -174,7 +174,8 @@
        (ruby +lsp
              +rails
              +tree-sitter)            ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust                         ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       (rust +lsp
+             +tree-sitter)            ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala                        ; java, but good
        ;;(scheme +guile)              ; a fully conniving family of lisps
        (sh +lsp
