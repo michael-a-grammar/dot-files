dd598873f6041a12ca58eee7fb6f8bda64fc4d2d
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-09-15 20:57:44 +0100

    Updates

--- scripts/symlinks.fish
+++ scripts/symlinks.fish
@@ -1,8 +1,13 @@
-ln -sf ~/dev/dot-files/.gitconfig                                 ~/.gitconfig
-ln -sf ~/dev/dot-files/.irbrc                                     ~/.irbrc
-ln -sf ~/dev/dot-files/starship.toml                              ~/.config/starship.toml
-ln -sf ~/dev/dot-files/bat/config                                 ~/.config/bat/config
-ln -sf ~/dev/dot-files/bat/themes/Catppuccin-mocha.tmTheme        ~/.config/bat/themes/Catppuccin-mocha.tmTheme
+ln -sf ~/dev/dot-files/.gitconfig    ~/.gitconfig
+ln -sf ~/dev/dot-files/.irbrc        ~/.irbrc
+ln -sf ~/dev/dot-files/starship.toml ~/.config/starship.toml
+ln -sf ~/dev/dot-files/bat/config    ~/.config/bat/config
+
+ln -sf ~/dev/dot-files/bat/themes/Catppuccin-frappe.tmTheme    ~/.config/bat/themes/Catppuccin-mocha.tmTheme
+ln -sf ~/dev/dot-files/bat/themes/Catppuccin-latte.tmTheme     ~/.config/bat/themes/Catppuccin-mocha.tmTheme
+ln -sf ~/dev/dot-files/bat/themes/Catppuccin-macchiato.tmTheme ~/.config/bat/themes/Catppuccin-macchiato.tmTheme
+ln -sf ~/dev/dot-files/bat/themes/Catppuccin-mocha.tmTheme     ~/.config/bat/themes/Catppuccin-mocha.tmTheme
+
 ln -sf ~/dev/dot-files/colorls/dark_colors.yaml                   ~/.config/colorls/dark_colors.yaml
 ln -sf ~/dev/dot-files/doom-emacs/config.el                       ~/.doom.d/config.el
 ln -sf ~/dev/dot-files/doom-emacs/init.el                         ~/.doom.d/init.el
@@ -19,8 +24,11 @@ ln -sf ~/dev/dot-files/fish/functions/P.fish                      ~/.config/fish
 ln -sf ~/dev/dot-files/fish/functions/V.fish                      ~/.config/fish/functions/V.fish
 ln -sf ~/dev/dot-files/helix/config.toml                          ~/.config/helix/config.toml
 ln -sf ~/dev/dot-files/k9s/skin.yml                               ~/.config/k9s/skin.yml
-ln -sf ~/dev/dot-files/kitty/Catppuccin-Mocha.conf                ~/.config/kitty/Catppuccin-Mocha.conf
-ln -sf ~/dev/dot-files/kitty/kitty.conf                           ~/.config/kitty/kitty.conf
-ln -sf ~/dev/dot-files/kitty/macos-launch-services-cmdline        ~/.config/kitty/macos-launch-services-cmdline
-ln -sf ~/dev/dot-files/lazygit/config.yml                         ~/Library/Application\ Support/lazygit/config.yml
-ln -sf ~/dev/dot-files/mc/ini                                     ~/.config/mc/ini
+
+ln -sf ~/dev/dot-files/kitty/Catppuccin-Latte.conf ~/.config/kitty/Catppuccin-Latte.conf
+ln -sf ~/dev/dot-files/kitty/Catppuccin-Mocha.conf ~/.config/kitty/Catppuccin-Mocha.conf
+
+ln -sf ~/dev/dot-files/kitty/kitty.conf                    ~/.config/kitty/kitty.conf
+ln -sf ~/dev/dot-files/kitty/macos-launch-services-cmdline ~/.config/kitty/macos-launch-services-cmdline
+ln -sf ~/dev/dot-files/lazygit/config.yml                  ~/Library/Application\ Support/lazygit/config.yml
+ln -sf ~/dev/dot-files/mc/ini                              ~/.config/mc/ini
