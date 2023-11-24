fd46e26211cfeff9c0952e115eeb061d494ee98b
mike-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2023-11-24 19:01:00 +0000

    Updates

--- scripts/symlinks.fish
+++ scripts/symlinks.fish
@@ -9,9 +9,9 @@ ln -sf ~/dev/dot-files/bat/themes/Catppuccin-macchiato.tmTheme ~/.config/bat/the
 ln -sf ~/dev/dot-files/bat/themes/Catppuccin-mocha.tmTheme     ~/.config/bat/themes/Catppuccin-mocha.tmTheme
 
 ln -sf ~/dev/dot-files/colorls/dark_colors.yaml                   ~/.config/colorls/dark_colors.yaml
-ln -sf ~/dev/dot-files/doom-emacs/config.el                       ~/.doom.d/config.el
-ln -sf ~/dev/dot-files/doom-emacs/init.el                         ~/.doom.d/init.el
-ln -sf ~/dev/dot-files/doom-emacs/package.el                      ~/.doom.d/package.el
+ln -sf ~/dev/dot-files/doom-emacs/config.el                       ~/.config/doom/config.el
+ln -sf ~/dev/dot-files/doom-emacs/init.el                         ~/.config/doom/init.el
+ln -sf ~/dev/dot-files/doom-emacs/package.el                      ~/.config/doom/package.el
 ln -sf ~/dev/dot-files/fish/config.fish                           ~/.config/fish/config.fish
 ln -sf ~/dev/dot-files/fish/functions/abbreviations.fish          ~/.config/fish/functions/abbreviations.fish
 ln -sf ~/dev/dot-files/fish/functions/fish_greeting.fish          ~/.config/fish/functions/fish_greeting.fish
