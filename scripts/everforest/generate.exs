8904f5da6a06750f3fee12cb098ad63ea4351e10
michael-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2025-05-30 15:37:39 +0100

    updates

---
+++ scripts/everforest/generate.exs
@@ -0,0 +1,53 @@
+Mix.install([
+  :jason
+])
+
+defmodule Theme do
+  @palettes %{
+    dark: "dark",
+    light: "light",
+    hard: "hard",
+    medium: "medium",
+    soft: "soft"
+  }
+
+  def palette(:dark = palette), do: get_palette(palette)
+  def palette(:light = palette), do: get_palette(palette)
+
+  def palette(:dark = palette, :hard = sub_palette), do: get_palette(palette, sub_palette)
+  def palette(:dark = palette, :medium = sub_palette), do: get_palette(palette, sub_palette)
+  def palette(:dark = palette, :soft = sub_palette), do: get_palette(palette, sub_palette)
+
+  def palette(:light = palette, :hard = sub_palette), do: get_palette(palette, sub_palette)
+  def palette(:light = palette, :medium = sub_palette), do: get_palette(palette, sub_palette)
+  def palette(:light = palette, :soft = sub_palette), do: get_palette(palette, sub_palette)
+
+  defp get_palette(palette) do
+    decode_json([@palettes[palette], "foreground.json"])
+  end
+
+  defp get_palette(palette, sub_palette) do
+    decode_json([@palettes[palette], "background", "#{@palettes[sub_palette]}.json"])
+  end
+
+  defp decode_json(paths) do
+    ["theme"] ++ paths
+    |> Path.join()
+    |> File.read!()
+    |> Jason.decode()
+  end
+end
+
+[:dark, :light]
+|> Enum.map(fn palette ->
+  foreground = Theme.palette(palette)
+
+  backgrounds = 
+    [:hard,:medium, :soft]
+    |> Enum.map(&(Theme.palette(palette, &1)))
+
+  {foreground, backgrounds}
+end)
+|> then(fn result ->
+  IO.inspect(result, label: "Result")
+end)
