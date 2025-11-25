4fd0c46ebd1e409bbbeac995ca0db08f9a40f1bc
michael-a-grammar <5898731+michael-a-grammar@users.noreply.github.com>
2025-11-25 09:33:06 +0000

    updates

--- fish/functions/voyager_get_layer.fish
+++ fish/functions/voyager_get_layer.fish
@@ -6,7 +6,9 @@ function voyager_get_layer
     else if test (string match --entire 'No keyboard connected' $kontroll_status)
         echo 'No keyboard connected'
     else if string match --regex --quiet '.*Current layer:\s+(?<current_layer>\d+)' $kontroll_status
-        echo $current_layer
+        set -l layer_icons 󰼭 󰎐  󰥠 󰍽
+
+        echo $layer_icons[(math $current_layer + 1)]
     else
         echo 'Unknown response from Kontroll'
     end
